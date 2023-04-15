fa8f72f55b3dc46a8af517b8ec6154e6374ab9b8
Puttipong Doungvichai <puttipong.dou@gmail.com>
2023-04-16 00:12:59 +0700

    update trim is work

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const multer = require('multer');
-const ffmpeg = require('ffmpeg');
-const path = require('path');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const ffmpeg = require("ffmpeg");
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
 
 const api = express();
 
-api.use(cors(
-  {
-    origin: 'https://pdouvch.com/api/*'
-  }
-));
+api.use(
+  cors({
+    origin: "https://pdouvch.com/api/*",
+  })
+);
 
 const corsOptions = {
-  origin: 'https://pdouvch.com/api/list',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  origin: "https://pdouvch.com/api/list",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // Set up Multer middleware to handle file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
@@ -29,107 +29,88 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-api.set('views', path.join(__dirname, 'views'));
-api.set('view engine', 'ejs');
+api.set("views", path.join(__dirname, "views"));
+api.set("view engine", "ejs");
 
 // Home page route
-api.get('/api', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+api.get("/api", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-
-api.get('/api/list', cors(corsOptions),(req, res) => {
+api.get("/api/list", cors(corsOptions), (req, res) => {
   // Read the contents of the uploads directory
-  fs.readdir('uploads', (err, files) => {
+  fs.readdir("uploads", (err, files) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('An error occurred');
+      return res.status(500).send("An error occurred");
     }
 
     // Filter the list to only include video files
-    const videoFiles = files.filter(file => {
+    const videoFiles = files.filter((file) => {
       const extension = path.extname(file).toLowerCase();
-      return extension === '.mp4' || extension === '.webm' || extension === '.ogg';
+      return (
+        extension === ".mp4" || extension === ".webm" || extension === ".ogg"
+      );
     });
 
     // Render the HTML page with the list of videos
-    res.render('index', { videos: videoFiles });
+    res.render("index", { videos: videoFiles });
   });
 });
 
 // Upload file route
-api.post('/api/upload', upload.single('video'), async (req, res) => {
+api.post("/api/upload", upload.single("video"), async (req, res) => {
+  const path = req.file.path;
   try {
-    const { mimetype, size } = req.file;
+    const video = new ffmpeg(path);
+    video.then(function (video) {
+      // Get video duration
+      const duration = video.metadata.duration.seconds;
 
-    // Check if uploaded file is a video and its size is less than or equal to 300MB
-    if (mimetype.startsWith('video/') && size <= 300000000) {
-      const video = await new ffmpeg('./ffmpeg');
-      // const video = await new ffmpeg(req.file.path);
-
-      // Get video duration in seconds
-      const duration = Math.ceil(video.metadata.duration.seconds);
-
-      // Check if video duration is more than 5 minutes
-      if (duration > 200) {
-        // Calculate number of parts to split the video into
-        const numParts = Math.ceil(duration / 200);
-
-        // Trim and split video file
-        for (let i = 0; i < numParts; i++) {
-          const startTime = i * 200;
-          const endTime = Math.min((i + 1) * 200, duration);
-
-          const outputFile = path.join(__dirname, `uploads/trimmed_${i}.mp4`);
-          await video.fnTranscode(outputFile, 'libx264', 'aac', {
-            t: 200,
-            start_time: startTime,
-            end_time: endTime,
+      // Trim the video if it's longer than 5 minutes
+      if (duration > 300) {
+        video
+          .setVideoDuration(300)
+          .save(`trimmeds/trimmed.mp4`, function (error, file) {
+            if (!error) {
+              console.log("Video trimmed successfully");
+              res.redirect("/api/list");
+            }
           });
-        }
-
-        // Send response indicating that video was uploaded and processed successfully
-        // res.send('Video was uploaded and processed successfully!');
-        res.redirect('/api/list');
       } else {
-        // Send response indicating that video was uploaded successfully but was not processed
-        // res.send('Video was uploaded successfully but was not processed because its duration is less than or equal to 5 minutes.');
-        res.redirect('/api/list');
+        console.log("Video uploaded successfully");
+        res.redirect("/api/list");
       }
-    } else {
-      // Send response indicating that uploaded file is not a video or its size is more than 300MB
-      res.send('Uploaded file is not a video or its size is more than 300MB.');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('An error occurred while processing the video.');
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Error occurred while trimming the video");
   }
 });
 
-
-api.get('/api/download/:filename', (req, res) => {
+api.get("/api/download/:filename", (req, res) => {
   const filePath = `${__dirname}/uploads/${req.params.filename}`;
-  
+
   if (!fs.existsSync(filePath)) {
-    return res.status(404).send('File not found.');
+    return res.status(404).send("File not found.");
   }
 
   res.download(filePath);
 });
 
-api.get('/api/delete/:filename', (req, res) => {
-  const folderPath = path.join(__dirname, 'uploads');
+api.get("/api/delete/:filename", (req, res) => {
+  const folderPath = path.join(__dirname, "uploads");
   const filePath = path.join(folderPath, req.params.filename);
-  fs.unlink(filePath, err => {
+  fs.unlink(filePath, (err) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Error deleting video');
+      return res.status(500).send("Error deleting video");
     }
     // res.send('Video deleted successfully');
-    res.redirect('/api/list');
+    res.redirect("/api/list");
   });
 });
 
 api.listen(80, () => {
-  console.log('Server is running on port 80');
+  console.log("Server is running on port 80");
 });
